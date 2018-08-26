8a6bfe53b2a384f5e1c517856fd05865dd587c11
MateuszGawel <mg.mateuszgawel@gmail.com>
2018-08-26 19:48:32 +0200

    init commit

---
+++ weselnedetale/src/main/java/com/weselnedetale/main/WeselnedetaleApplication.java
@@ -0,0 +1,12 @@
+package com.weselnedetale.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeselnedetaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeselnedetaleApplication.class, args);
+	}
+}
